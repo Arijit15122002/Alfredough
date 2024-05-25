cd4232316f4dd5e6758841056e0f046c353cdc85
Arijit15122002 <biswasarijit2002@gmail.com>
2024-05-25 22:33:30 +0530

    Fifth commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,6 @@ const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}, {
-    origin: ["https://deploy.mern.lwhq.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
 }))
 
 //configuration
